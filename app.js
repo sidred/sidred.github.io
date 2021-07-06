4a5cdf342e78cd3dfeb1823d0fcca351c6ca8c82
sid <sidred@gmail.com>
2021-07-06 07:53:46 +0530

    update

--- app.js
+++ app.js
@@ -1,11 +1,42 @@
 function systemClick() {
-    document.getElementById('window-system').addEventListener("click", function changeContent() {
-      window.open('https://www.sensibull.com', '_system');
-    });
+    window.open('https://www.sensibull.com', '_system');
 }
 
 function blankClick() {
-    document.getElementById('window-blank').addEventListener("click", function changeContent() {
-      window.open('https://www.sensibull.com', '_blank');
-    });
+    // document.getElementById('window-blank').addEventListener("click", function changeContent() {
+    //   window.open('https://www.sensibull.com', '_blank');
+    // });
+    // window.open('https://www.sensibull.com', '_blank');
+    openLink()
+}
+
+
+function openLink() {
+
+  const standalone = window.navigator.standalone;
+  const userAgent = window.navigator.userAgent.toLowerCase();
+  const safari = /safari/.test(userAgent);
+  const ios = /iphone|ipod|ipad/.test(userAgent);
+
+  let qq;
+  let url = 'https://www.sensibull.com'
+
+  if (ios) {
+    if (!standalone && !safari) {
+        // iOS webview
+        window.close(); // Close the empty view window
+        window.open(url, '_system', 'location=yes', replace);
+        return
+    }
+  } else {
+    if (userAgent.includes('wv')) {
+        // Android webview
+        window.close(); // Close the empty view window
+        const winloc = 'intent:' + url + '#Intent;end';
+        window.location = winloc;
+        return
+    }
+  }
+  window.open(url, '_blank');
+
 }
