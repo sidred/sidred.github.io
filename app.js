6279f9b282a8120cde398d649a3fcfbeb9b7b35c
sid <sidred@gmail.com>
2021-07-06 07:35:27 +0530

    update

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
-document.getElementById('window-system').onclick = function changeContent() {
+document.getElementById('window-system').addEventListener("click", function changeContent() {
    window.open('https://www.sensibull.com', '_system');
-}
-document.getElementById('window-blank').onclick = function changeContent() {
+});
+document.getElementById('window-blank').addEventListener("click", function changeContent() {
    window.open('https://www.sensibull.com', '_blank');
-}
+});
