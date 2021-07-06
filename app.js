97965ef8c5e0b1be64b54dc63a577448c82c4602
sid <sidred@gmail.com>
2021-07-06 07:38:49 +0530

    update

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
-document.getElementById('window-system').addEventListener("click", function changeContent() {
-   window.open('https://www.sensibull.com', '_system');
-});
-document.getElementById('window-blank').addEventListener("click", function changeContent() {
-   window.open('https://www.sensibull.com', '_blank');
-});
+function systemClick() {
+    document.getElementById('window-system').addEventListener("click", function changeContent() {
+      window.open('https://www.sensibull.com', '_system');
+    });
+}
+
+function blankClick() {
+    document.getElementById('window-blank').addEventListener("click", function changeContent() {
+      window.open('https://www.sensibull.com', '_blank');
+    });
+}
