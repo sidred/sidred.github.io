0a99c62a64e682849d41ea56a715b8de8e83dec8
sid <sidred@gmail.com>
2021-07-06 07:31:47 +0530

    update

--- app.js
+++ app.js
@@ -1,3 +1,6 @@
 document.getElementById('window-system').onclick = function changeContent() {
    window.open('https://www.sensibull.com', '_system');
 }
+document.getElementById('window-blank').onclick = function changeContent() {
+   window.open('https://www.sensibull.com', '_blank');
+}
